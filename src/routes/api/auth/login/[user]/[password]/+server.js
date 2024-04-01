b8d063ba746de1d6f219301e7fda47af6107cc3a
JA-Dilao <69741368+Cuject@users.noreply.github.com>
2024-04-02 05:35:03 +0800

    auth login logic

--- src/routes/api/auth/login/[user]/[password]/+server.js
+++ src/routes/api/auth/login/[user]/[password]/+server.js
@@ -1,27 +1,29 @@
 import { json } from "@sveltejs/kit";
+import { sha256, sha224 } from 'js-sha256';
+import {MongoClient} from 'mongodb';
+import { SECRET_URI, DB_NAME } from '$env/static/private'; 
 
 export async function GET(RequestEvent){
     const { params } = RequestEvent;
     const { user, password } = params;
     
-    /*client = new MongoClient(SECRET_URI);
+    client = new MongoClient(SECRET_URI);
     db = client.db(DB_NAME);
 
     const users = await db.collection("users").find().toArray();
     const user_emails = users.map((user_email) => {return user_email.email})
     const user_hashes = users.map((user_hash) => {return user_hash.hash})
 
-    /*
 
     if (!email || !password) {
         return json({message: "Missing email or password", logged_in: false, user: user})
     }
     
-    if(!user_emails.includes(email))
+    if(!user_emails.includes(email)){
 
         return json({message: "User doesn't exist", logged_in: false, user: user})
 
-        }else if(sha224(email+password) === user_hashes[user_emails.indexOf(email)]){
+    }else if(sha224(email+password) === user_hashes[user_emails.indexOf(email)]){
 
             return json({message: 'Logged in', logged_in: true, user: user})
            //(email);
@@ -30,7 +32,7 @@ export async function GET(RequestEvent){
             return json({message: "Incorrect email or password", logged_in: false, user: user})
     }
 
-    return json({message: 'Logged in', logged_in: true, user: user})*/
-    return json({message: "Log In", user:user, password:password})
+    return json({message: 'Logged in', logged_in: true, user: user})
+    
 
 }
