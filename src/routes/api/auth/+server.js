727c6180d5e612cda3d02c253770fc019eb74d24
JA-Dilao <69741368+Cuject@users.noreply.github.com>
2024-04-02 05:26:07 +0800

    user field

--- src/routes/api/auth/+server.js
+++
@@ -1,9 +0,0 @@
-import { json } from "@sveltejs/kit";
-import {MongoClient} from 'mongodb';
-import { SECRET_URI, DB_NAME } from '$env/static/private';
-
-export async function GET(RequestEvent){
-    
-    return json({message: "Log In"})
-
-}
---
+++ src/routes/api/auth/login/[user]/+server.js
@@ -0,0 +1,36 @@
+import { json } from "@sveltejs/kit";
+
+export async function GET(RequestEvent){
+    const { params } = RequestEvent;
+    const { user } = params;
+    
+    /*client = new MongoClient(SECRET_URI);
+    db = client.db(DB_NAME);
+
+    const users = await db.collection("users").find().toArray();
+    const user_emails = users.map((user_email) => {return user_email.email})
+    const user_hashes = users.map((user_hash) => {return user_hash.hash})
+
+    /*
+
+    if (!email || !password) {
+        return json({message: "Missing email or password", logged_in: false, user: user})
+    }
+    
+    if(!user_emails.includes(email))
+
+        return json({message: "User doesn't exist", logged_in: false, user: user})
+
+        }else if(sha224(email+password) === user_hashes[user_emails.indexOf(email)]){
+
+            return json({message: 'Logged in', logged_in: true, user: user})
+           //(email);
+        }else{
+
+            return json({message: "Incorrect email or password", logged_in: false, user: user})
+    }
+
+    return json({message: 'Logged in', logged_in: true, user: user})*/
+    return json({message: "Log In", user:user})
+
+}
