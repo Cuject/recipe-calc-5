d7f3fddf4a2fc53e1fb9de3c1d90486765ccd030
JA-Dilao <69741368+Cuject@users.noreply.github.com>
2024-04-13 06:25:52 +0800

    test returns for add edit delete

--- src/routes/api/recipe/[command]/[user]/[recipeName]/[foodQty]/+server.js
+++ src/routes/api/recipe/[command]/[user]/[recipeName]/[foodQty]/+server.js
@@ -4,25 +4,26 @@ import { SECRET_URI, DB_NAME } from '$env/static/private';
 
 export async function GET(RequestEvent){
     const { params } = RequestEvent;
-    const { user, command, recipeName } = params;
+    const { user, command, recipeName, foodQty } = params;
     
     //const client = new MongoClient(SECRET_URI)
     //const db = client.db(DB_NAME)
     //const recipes = await db.collection('recipes').find({user: user}).toArray()
     if (command == "add"){
-        return json({user: user, command: command, message: "added " + String(recipeName)})
+        return json({user: user, command: command, message: "added " + String(foodQty)})
     }
 
     if (command == "edit"){
-        return json({user: user, command: command, message: "changed " + String(recipeName)})
+        return json({user: user, command: command, message: "changed " + String(foodQty)})
     }
 
     if (command == "delete"){
-        return json({user: user, command: command, message: "deleted " + String(recipeName)})
+        return json({user: user, command: command, message: "deleted " + String(foodQty)})
     }
 
 
-    return json({user: user, command: command, recipeName: recipeName})
+
+    return json({user: user, command: command, recipeName: recipeName, foodQty: foodQty})
 
 
 
